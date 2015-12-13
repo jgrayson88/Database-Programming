48d826f9529ff63e4f8202d685dc2d64e21d337c
Janina Grayson <jgrays01@nyit.edu>
2015-12-12 20:23:38 -0500

    project complete except for messed up cart.jsp screen

---
+++ build/generated-sources/ap-source-output/murach/business/User_.java
@@ -0,0 +1,21 @@
+package murach.business;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-12T17:04:22")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> firstName;
+    public static volatile SingularAttribute<User, String> lastName;
+    public static volatile SingularAttribute<User, String> userPassword;
+    public static volatile SingularAttribute<User, String> gender;
+    public static volatile SingularAttribute<User, String> phone;
+    public static volatile SingularAttribute<User, String> birth;
+    public static volatile SingularAttribute<User, String> userName;
+    public static volatile SingularAttribute<User, Long> userId;
+    public static volatile SingularAttribute<User, String> email;
+
+}
